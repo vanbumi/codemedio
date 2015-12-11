40216e0005c8f166a96074dfac575fa0752cb3d3
vanbumi <jsp.dio@gmail.com>
2015-12-11 18:41:44 +0700

    modif single post

--- app/controllers/single_post_controller.rb
+++ app/controllers/single_post_controller.rb
@@ -7,7 +7,7 @@ class SinglePostController < ApplicationController
   def index
   	
 	slug = params[:slug]
-    @postsL = Post.where("slug = ?",slug).limit(1)
+    @postSing = Post.where("slug = ?",slug).limit(1)
 
   end
 
